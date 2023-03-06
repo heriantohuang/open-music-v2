77ea6cab9d6e157f86692bce2d3d05ae399af850
herianto <herianto.bti@panin.co.id>
2023-03-06 10:08:25 +0700

    initial commit

---
+++ src/validator/albums/schema.js
@@ -0,0 +1,9 @@
+/* eslint-disable newline-per-chained-call */
+const Joi = require('joi');
+
+const AlbumPayloadSchema = Joi.object({
+  name: Joi.string().required(),
+  year: Joi.number().integer().min(1970).max(new Date().getFullYear()).required(),
+});
+
+module.exports = { AlbumPayloadSchema };
